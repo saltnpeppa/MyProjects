48ae23fade195ba046a441651f1d88aef1c58a8d
Sidhant Rajadnya <r03.sidhant@gmail.com>
2018-08-13 22:20:48 -0700

    problem 6

---
+++ 6.kt
@@ -0,0 +1,12 @@
+package ProjectEuler
+
+fun main(args: Array<String>) {
+    var list = arrayListOf<Int>()
+    for (i in 1..100){
+        list.add(i * i)
+    }
+
+    println(5050*5050 -(list.sum()))
+
+
+}
