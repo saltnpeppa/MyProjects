33ebe9a039ed8fa1a553899b04dc807d1f2399b6
Sidhant Rajadnya <r03.sidhant@gmail.com>
2018-11-02 15:39:05 -0700

    Number 10

---
+++ 10.kt
@@ -0,0 +1,19 @@
+package ProjectEuler
+
+import kotlin.system.measureTimeMillis
+
+fun otherPrimes(): List<Int> {
+    var list = arrayListOf<Int>()
+    for (i in 1..20000){
+        if (i % 2 != 0) {
+            if ((getFactor(i.toLong())).size == 2) {
+                list.add(i)
+            }
+        }
+    }
+    return list
+}
+
+fun main(args: Array<String>) {
+    println((otherPrimes().sum().toLong()))
+}
